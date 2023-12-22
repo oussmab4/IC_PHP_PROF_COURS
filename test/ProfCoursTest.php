387d403ff1ef3fdc947d6816cf29d5f4f86a8203
oussmab4 <oussama.mabsout@edu.esiee.fr>
2023-12-22 17:22:32 +0100

    Exo finit

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -74,12 +74,28 @@ class ProfCoursTest extends TestCase
             new Prof("Nom_prof1", "Prenom_prof1", "10/01/1982", "lieu_prof1"),      // idprof = 1
             new Prof("Nom_prof2", "Prenom_prof2", "10/02/1982", "lieu_prof2"),      // idprof = 2
             new Prof("Nom_prof3", "Prenom_prof3", "10/03/1982", "lieu_prof3"),      // idprof = 3
+            new Prof("Nom_prof4", "Prenom_prof4", "10/04/1982", "lieu_prof4"),      // idprof = 4
+            new Prof("Nom_prof5", "Prenom_prof5", "10/05/1982", "lieu_prof5"),      // idprof = 5
+            new Prof("Nom_prof6", "Prenom_prof6", "10/06/1982", "lieu_prof6"),      // idprof = 6
+            new Prof("Nom_prof7", "Prenom_prof7", "10/07/1982", "lieu_prof7"),      // idprof = 7
+            new Prof("Nom_prof8", "Prenom_prof8", "10/08/1982", "lieu_prof8"),      // idprof = 8       ** A SUPPRIMER **
+            new Prof("Nom_prof9", "Prenom_prof9", "10/09/1982", "lieu_prof9"),      // idprof = 9
+            new Prof("Nom_prof10", "Prenom_prof10", "10/10/1982", "lieu_prof10")    // idprof = 10      ** A MODIFIER **
         ];
 
         self::$cours_a = [
-            new Cours("Iot", "10", 1),       // idcours = 1
-            new Cours("IA", "12", 3),     // idcours = 2
-            new Cours("EDL", "5", 6),       // idcours = 3
+            new Cours("Cours1", "2", 1),       // idcours = 1
+            new Cours("Cours2", "2.5", 3),     // idcours = 2
+            new Cours("Cours3", "3", 5),       // idcours = 3
+            new Cours("Cours4", "2", 3),       // idcours = 4
+            new Cours("Cours5", "3", 3),       // idcours = 5
+            new Cours("Cours6", "2", 4),       // idcours = 6
+            new Cours("Cours7", "3", 5),       // idcours = 7   ** A SUPPRIMER **
+            new Cours("Cours8", "4", 5),       // idcours = 8
+            new Cours("Cours9", "3", 5),        // idcours = 9   ** A MODIFIER **
+            new Cours("Iot", "10", 1), 
+            new Cours("IA", "12", 3), 
+            new Cours("EDL", "5", 6), 
             
             /**
             *
